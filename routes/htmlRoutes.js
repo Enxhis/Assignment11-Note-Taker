dcec04c92cf15dbed46ca717b07b2937d141162d
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-20 13:14:26 -0500

    fixed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,12 +6,12 @@ module.exports = function(app){
     // HTML GET requests
     // Route to notes
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
     });
 
     // Route to homepage
     app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
     });
 
 }
