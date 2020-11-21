a916d6e37d6130113c4d0a909cd848ca254c4462
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-21 13:15:18 -0500

    upadated POST apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,17 @@ var notesDatabase = require("../Develop/db/db.json");
 module.exports = function(app){
     // API GET request to retrieve data from database
     app.get("/api/notes", function(req, res){
-        res.json(notesDatabase);
+        res.send(notesDatabase);
     });
 
     // API POST to create new notes
     app.post("/api/notes", function(req, res){
-        res.json(true);
+        var newNote = {
+            title : req.body.title,
+            text: req.body.text
+        };
+        notesDatabase.push(newNote);
+        res.send(newNote);
     });
 
     // API DELETE to delete notes
