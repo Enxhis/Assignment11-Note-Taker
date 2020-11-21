1653a806239a3683b868bd1415b30241a4f26a95
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-21 13:56:46 -0500

    added ID for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,36 @@
 // Load Data
 var fs = require("fs");
-var notesDatabase = require("../Develop/db/db.json");
+var db = require("../Develop/db/db.json");
+// Generate a v4 (random) id
+var uuidv4 = require("uuid");
 
 // Routing
 module.exports = function(app){
     // API GET request to retrieve data from database
     app.get("/api/notes", function(req, res){
-        res.send(notesDatabase);
+        res.send(db);
     });
 
     // API POST to create new notes
     app.post("/api/notes", function(req, res){
         var newNote = {
+            id: uuidv4(),
             title : req.body.title,
             text: req.body.text
         };
-        notesDatabase.push(newNote);
+        db.push(newNote);
         res.send(newNote);
     });
 
     // API DELETE to delete notes
     app.delete("/api/notes/:id", function(req, res){
-        res.send("DELETE request");
+        var notesID = req.params.id;
+        for(var i = 0; i < db.length; i++){
+            if(db[i].id === notesID){
+                var index = db.indexOf(db);
+                var deletedNote = db.slice(index, 1);
+            }
+        }
+        res.send(deletedNote);
     })
 }
