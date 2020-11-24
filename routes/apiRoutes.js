05e769b80dc9d9b8004d195878c6cc59c610e8bb
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-23 23:09:09 -0500

    full functionality before deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,34 +8,37 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = function (app) {
     // API GET request to retrieve data from database
     app.get("/api/notes", function (req, res) {
-        res.send(db);
+        res.json(db);
     });
 
     // API POST to create new notes
     app.post("/api/notes", function (req, res) {
-        var newNote = {
-            id: uuidv4(),
-            title: req.body.title,
-            text: req.body.text
-
-        };
-        // push newNote to database
+        req.body.id = uuidv4();
+        var newNote = req.body
+        //pushing the new note to the JSON file on memory          
         db.push(newNote);
-        res.send(newNote);
+        // write db JSON file
+        fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
+            if (err) throw err;
+            // else send the response
+            res.json(db);
+        });
     });
 
     // API DELETE to delete notes
     app.delete("/api/notes/:id", function (req, res) {
         // id variable
         var noteID = req.params.id
-
         for (var i = 0; i < db.length; i++) {
-            // check that the noteId matches the id in the database
-            if (db[i].id === noteID) {
-                let index = db.indexOf(db);
-                db.splice(index, 1);
-            }
-            res.send(db);
+            if (noteID === db[i].id) {
+                db.splice(i, 1);
+            };
         }
+        //writing the new db with the deleted note  
+        fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
+            if (err) throw err;
+            // else send the response
+            res.json(db);
+        });
     });
 }
